015b3d1010b582213686375e96221fdfdfa05795
Timur999 <46047950+Timur999@users.noreply.github.com>
2019-02-20 17:02:34 +0100

    added char

--- src/app/model/root.ts
+++ src/app/model/root.ts
@@ -1,5 +1,5 @@
 export class Root {
-    // root = "http://trletsrun.azurewebsites.net"; 
+    // root = "http://trletsrun.azurewebsites.net"; s
     static rootUrl: string = "http://localhost:63062";
     constructor() {
         Root.rootUrl = "http://localhost:63062";
