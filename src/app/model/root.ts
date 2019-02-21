6a6ab18c1d550e20af3617df2278490f548cbad0
Timur999 <46047950+Timur999@users.noreply.github.com>
2019-02-21 12:44:53 +0100

    removed char

--- src/app/model/root.ts
+++ src/app/model/root.ts
@@ -1,5 +1,5 @@
 export class Root {
-    // root = "http://trletsrun.azurewebsites.net"; ss
+    // root = "http://trletsrun.azurewebsites.net"; 
     static rootUrl: string = "http://localhost:63062"; 
     constructor() {
         Root.rootUrl = "http://localhost:63062"; 
