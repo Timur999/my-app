7dd93c4589ccb1ba26b9cf0afde072d9f0321148
Timur999 <46047950+Timur999@users.noreply.github.com>
2019-02-21 13:12:18 +0100

    .gitignore is now working

--- src/app/model/root.ts
+++
@@ -1,7 +0,0 @@
-export class Root {
-    // root = "http://localhost:63062"; 
-    static rootUrl: string = "http://trletsrun.azurewebsites.net";
-    constructor() {
-        Root.rootUrl = "http://trletsrun.azurewebsites.net";
-    }
-}
