b642319543f5a2e0d7e64757dc9e7aed3a9f4d99
Timur999 <46047950+Timur999@users.noreply.github.com>
2019-02-21 01:19:11 +0100

    delete char

--- src/app/model/root.ts
+++ src/app/model/root.ts
@@ -1,5 +1,5 @@
 export class Root {
-    // root = "http://trletsrun.azurewebsites.net"; s
+    // root = "http://trletsrun.azurewebsites.net"; 
     static rootUrl: string = "http://localhost:63062"; 
     constructor() {
         Root.rootUrl = "http://localhost:63062"; 
